3990a87a40542bd00a5ad6049c9f8715aee1493b
Drusseldorf <150291270+Drusseldorf@users.noreply.github.com>
2024-04-23 18:21:33 +0300

    Update test_actions_env.py

--- tests/test_actions_env.py
+++ tests/test_actions_env.py
@@ -4,3 +4,4 @@ from config.base_settings import project_settings
 def test_show_env():
     qwe = project_settings.api.operators.user_name
     print(qwe)
+    assert qwe == 'andkor_oper1@gmail.com_ENV'
